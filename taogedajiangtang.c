d86a1fd5b8cfac6ea66a4fd1277b0811c16a6474
IceWitch <359023320@qq.com>
2018-06-19 22:05:52 +0800

    [git-learn-diary] 2018/4/16 Signed-off-by: IceWitch <359023320@qq.com>

---
+++ taogedajiangtang.c
@@ -0,0 +1,17 @@
+<涛哥git学习日记>
+【2018.4.16】
+1.git repo gerrit三者关系
+git一次只能管理一个仓库
+repo可以管理多个仓库
+gerrit只是一个网页管理工具
+2.git本地仓库/远程仓库/分布式版本管理/集中式版本管理
+git是分布式版本管理工具,具有本地仓库
+但是也有一个服务器管理提交
+3.文件状态
+untracked
+tracked(staged, modified, unmodified)
+4.git分支
+分支是指向提交对象的可变指针
+(HEAD->分支->commit)
+5.git版本号
+哈希值,一般取前7位
